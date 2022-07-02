93c6d31fc70cbe030f7074a7a54548d777d91cbd
normanade <enoze@foxmail.com>
2022-07-02 14:38:29 +0800

    reduce log amount

--- examples/node.rs
+++ examples/node.rs
@@ -16,7 +16,7 @@ const CONFIG_PATH: &str = "node.ini";
 
 fn main() -> Result<(), Box<dyn Error>> {
     std::env::set_var("RUST_BACKTRACE", "1");
-    std::env::set_var("RUST_LOG", "trace");
+    std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
     let conf = Conf::new(CONFIG_PATH);
